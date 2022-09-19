1343d3a8ab44eafeff028eb379772c3dd3a0e2f4
tineocac <carlostineocac@gmail.com>
2022-09-19 15:31:11 -0400

    styles

--- src/components/UsersList.jsx
+++ src/components/UsersList.jsx
@@ -1,23 +1,25 @@
 import axios from "axios";
 
-const UsersList = ({users, selectUser, getUsers, setIsLoading}) => {
+const UsersList = ({ users, selectUser, getUsers, setIsLoading }) => {
 
     const deleteUser = (id) => {
         axios.delete(`https://users-crud1.herokuapp.com/users/${id}/`)
-        .then( () => getUsers())
+            .then(() => getUsers())
         setIsLoading(true)
     }
 
     return (
-        <ul>
+        <ul className="ul-container">
             {
-                users.map( user => (
-                    <li key={user.id}>
+                users.map(user => (
+                    <li className="li-container" key={user.id}>
                         <h2>{user.first_name} {user.last_name}</h2>
                         <small>{user.email}</small>
                         <b><i className="fa-solid fa-cake-candles"></i>{user.birthday}</b>
-                        <button onClick={() => selectUser(user)}>Edit</button>
-                        <button onClick={() => deleteUser(user.id)}>Delete</button>
+                        <div className="buttons-list">
+                            <button onClick={() => selectUser(user)}>Edit</button>
+                            <button onClick={() => deleteUser(user.id)}>Delete</button>
+                        </div>
                     </li>
                 ))
             }
