81a46fa01029ce4599afbcff53bfb0a50efd60e5
tineocac <carlostineocac@gmail.com>
2022-09-21 17:53:51 -0400

    corretions modal

--- src/components/UsersForm.jsx
+++ src/components/UsersForm.jsx
@@ -1,7 +1,6 @@
 import axios from "axios";
 import { useEffect } from "react";
 import { useForm } from "react-hook-form";
-import {Modal} from 'reactstrap'
 
 const UsersForm = ({ getUsers, userSelected, deselectUser, setIsLoading }) => {
 
@@ -40,33 +39,31 @@ const UsersForm = ({ getUsers, userSelected, deselectUser, setIsLoading }) => {
     }
 
     return (
-        <Modal>
-            <form className="form-container" onSubmit={handleSubmit(submit)}>
-                <h1 className="form-title" >New User</h1>
-                <div className="input-container">
-                    <label htmlFor="first_name"><i className="fa-solid fa-user"></i></label>
-                    <input type="text" id="first_name" placeholder="first name"  {...register("first_name")} />
-                    <label htmlFor="last_name"></label>
-                    <input className="input-last_name" type="text" id="last_name" placeholder="last name"  {...register("last_name")} />
-                </div>
-                <div className="input-container">
-                    <label htmlFor="email"><i className="fa-solid fa-envelope"></i></label>
-                    <input type="text" id="email" placeholder="email"  {...register("email")} />
-                </div>
-                <div className="input-container">
-                    <label htmlFor="password"><i className="fa-solid fa-lock"></i></label>
-                    <input type="password" id="password" placeholder="password"  {...register("password")} />
-                </div>
-                <div className="input-container">
-                    <label htmlFor="birthday"><i className="fa-solid fa-cake-candles"></i></label>
-                    <input type="date" id="birthday"  {...register("birthday")} />
-                </div>
-                <div className="buttons">
-                    <button className="button">{userSelected ? 'Update' : 'Submit'}</button>
-                    <button className="button clear" type="Button" onClick={clear}>Clear</button>
-                </div>
-            </form>
-        </Modal>
+        <form className="form-container" onSubmit={handleSubmit(submit)}>
+            <h1 className="form-title" >New User</h1>
+            <div className="input-container">
+                <label htmlFor="first_name"><i className="fa-solid fa-user"></i></label>
+                <input type="text" id="first_name" placeholder="first name"  {...register("first_name")} />
+                <label htmlFor="last_name"></label>
+                <input className="input-last_name" type="text" id="last_name" placeholder="last name"  {...register("last_name")} />
+            </div>
+            <div className="input-container">
+                <label htmlFor="email"><i className="fa-solid fa-envelope"></i></label>
+                <input type="text" id="email" placeholder="email"  {...register("email")} />
+            </div>
+            <div className="input-container">
+                <label htmlFor="password"><i className="fa-solid fa-lock"></i></label>
+                <input type="password" id="password" placeholder="password"  {...register("password")} />
+            </div>
+            <div className="input-container">
+                <label htmlFor="birthday"><i className="fa-solid fa-cake-candles"></i></label>
+                <input type="date" id="birthday"  {...register("birthday")} />
+            </div>
+            <div className="buttons">
+                <button className="button">{userSelected ? 'Update' : 'Submit'}</button>
+                <button className="button clear" type="Button" onClick={clear}>Clear</button>
+            </div>
+        </form>
     );
 };
 
